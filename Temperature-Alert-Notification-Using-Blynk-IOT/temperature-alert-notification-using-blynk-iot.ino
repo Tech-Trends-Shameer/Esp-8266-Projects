2ac7c8e12af3498cbd21255d1c63016bb5fc3903
shameermohamed <shameer50@gmail.com>
2022-04-03 10:59:42 +0530

    Create temperature-alert-notification-using-blynk-iot.ino

---
+++ Temperature-Alert-Notification-Using-Blynk-IOT/temperature-alert-notification-using-blynk-iot.ino
@@ -0,0 +1,55 @@
+#define BLYNK_TEMPLATE_ID "TemplateID"
+#define BLYNK_DEVICE_NAME "Temperature Alert"
+#define BLYNK_AUTH_TOKEN "Auth Token"
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+ 
+
+#include <DHT.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "WiFi Username";  // type your wifi name
+char pass[] = "WiFi Password";  // type your wifi password
+
+#define DHTPIN 2          // Mention the digital pin where you connected 
+#define DHTTYPE DHT11     // DHT 11  
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+void sendSensor(){
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.println(t);
+  Blynk.virtualWrite(V5, h);
+  Blynk.virtualWrite(V6, t);
+    Serial.print("Temperature : ");
+    Serial.print(t);
+    Serial.print("    Humidity : ");
+    Serial.println(h);
+
+
+  if(t > 30){
+   // Blynk.email("shameer50@gmail.com", "Alert", "Temperature over 28C!");
+    Blynk.logEvent("temp_alert","Temp above 30 degree");
+  }
+}
+
+void setup(){
+   Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+  timer.setInterval(2500L, sendSensor);
+}
+
+void loop(){
+  Blynk.run();
+  timer.run();
+}
