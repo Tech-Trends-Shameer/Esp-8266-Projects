924752e58f0804ea4aa1561a26e7e91db9e72188
shameermohamed <shameer50@gmail.com>
2022-10-27 18:29:32 +0530

    Create GoogleScript.js

---
+++ Read-Data-From-Google-Spreadsheet-and-Display-in-OLED/GoogleScript.js
@@ -0,0 +1,11 @@
+ 
+var ss = SpreadsheetApp.openById('192k4yM-OrKsxU4E9w7pjkdk0OMbV2k0HZARcqkNzI8g');
+var sheet = ss.getSheetByName('Sheet1');
+ 
+function doGet(e){
+  var read = e.parameter.read;
+
+  if (read !== undefined){
+    return ContentService.createTextOutput(sheet.getRange('A1').getValue());
+  }
+}
