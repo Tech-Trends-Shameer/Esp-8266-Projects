c1356837279edc859a32e30a1ac5dd2eea6f75cb
shameermohamed <shameer50@gmail.com>
2022-10-27 18:30:13 +0530

    Update GoogleScript.js

--- Read-Data-From-Google-Spreadsheet-and-Display-in-OLED/GoogleScript.js
+++ Read-Data-From-Google-Spreadsheet-and-Display-in-OLED/GoogleScript.js
@@ -1,5 +1,5 @@
  
-var ss = SpreadsheetApp.openById('192k4yM-OrKsxU4E9w7pjkdk0OMbV2k0HZARcqkNzI8g');
+var ss = SpreadsheetApp.openById('192k4yM-OrKsxU4E9w7pjkdk0OsdghhffjyjkkkMbV2k0HZARcqkNzI8g');
 var sheet = ss.getSheetByName('Sheet1');
  
 function doGet(e){
