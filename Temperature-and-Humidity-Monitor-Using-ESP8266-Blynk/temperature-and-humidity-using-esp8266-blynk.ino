29f52c318bb448d8a54e66e1780e9f66ecc6d9e6
shameermohamed <shameer50@gmail.com>
2022-11-14 21:46:15 +0530

    Create temperature-and-humidity-using-esp8266-blynk.ino

---
+++ Temperature-and-Humidity-Monitor-Using-ESP8266-Blynk/temperature-and-humidity-using-esp8266-blynk.ino
@@ -0,0 +1,59 @@
+#define BLYNK_TEMPLATE_ID "TemplateID"
+#define BLYNK_DEVICE_NAME "Temperature and Humidity Monitor"
+#define BLYNK_AUTH_TOKEN "AuthToken"
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h> 
+#include <BlynkSimpleEsp8266.h>
+
+#include <DHT.h>
+
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "Wifi Username";  // type your wifi name
+char pass[] = "Wifi Password";  // type your wifi password
+
+BlynkTimer timer;
+
+
+#define DHTPIN 4 //Connect Out pin to D2 in NODE MCU
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+    Blynk.virtualWrite(V0, t);
+    Blynk.virtualWrite(V1, h);
+    Serial.print("Temperature : ");
+    Serial.print(t);
+    Serial.print("    Humidity : ");
+    Serial.println(h);
+}
+void setup()
+{   
+  
+  Serial.begin(115200);
+  
+
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+  timer.setInterval(100L, sendSensor);
+ 
+  }
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+ }
